9629445f6cdba40123b692678ff40a6899067c74
Victor Toniato <143565396+venckszz@users.noreply.github.com>
2026-07-03 22:42:53 -0300

    Delete 'Pagina' struct definition from fotos.c
    Removed the definition of the 'Pagina' struct.

--- fotos.c
+++ fotos.c
@@ -1,11 +1,1 @@
-typedef struct pagina {
-    bool pagina_ativa;
-    int pos_prox_livre;
-    int posPagina;
-    int qtd_chaves_atuais;
-    bool ehFolha;
-    int posFilhos[ORDEM + 1];
-    int posProximo;
-    int posRegistro[ORDEM];
-    void* chaves[ORDEM]; 
-} Pagina;
+
